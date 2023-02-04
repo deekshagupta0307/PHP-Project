ea7bc6d55da5b46536a0b53656420ad86fac5d47
Deeksha <109609171+deekshagupta0307@users.noreply.github.com>
2023-02-04 18:40:09 +0530

    Add files via upload

---
+++ code.php
@@ -0,0 +1,40 @@
+<html>
+<head>
+<title> PHP Webpage </title>
+</head>
+<body>
+       <center>
+       <h1>Festivals and their Description</h1>
+       <table border="1">
+               <th>Image</th>
+               <th>Description</th>
+
+<tr>
+<td><img src = "images/Diwali.jpg" width="200" height="200"></td>
+<td><?php echo "For Hindus, this is linked to the ancient legend of Lord Rama,<br> who was deprived of his kingdom and sent into exile for 14 years.<br>
+<b><u>Diwali</u></b> celebrates Rama's eventual defeat of the evil spirit <br> Ravana, and his triumphant return to his home.";?></td>
+</tr>
+
+<tr>
+<td><img src ="images/Holi.jpg" width="200" height="200"></td>
+<td><?php echo "The <b><u>Holi</u></b> Festival is celebrated as a way to welcome in spring.<br> It is said that during the Holi Festival, the gods turn a <br>
+ blind eye, and it's one of the few times extremely <br> devout Hindus allow themselves to let loose.";?></td>
+</tr>
+
+<tr>
+<td><img src="images/Raksha Bandhan.jpg" width="200" height="200"></td>
+<td><?php echo "<b><u>Raksha Bandhan</u></b> is meant to celebrate any type of brother-sister relationship <br> between men and women who may not be biologically 
+related. <br> On this day, a sister ties a rakhi around the wrist of her brother in order <br> to pray for his prosperity, health and well-being.";?></td>
+</tr>
+
+<tr>
+<td><img src="images/Navratri.jpg" width="200" height="200"></td>
+<td><?php echo "<b><u>Navratri</u></b> is celebrated for nine nights.Navratri is a time when Hindus <br> celebrate the goddess Durga for killing the demon,
+ Mahishasura.<br> A more powerful God, Lord Brahma, had blessed Mahishasura with the gift of <br> immortality because of his dedication to him,
+ which meant he could never die.";?></td>
+</tr>
+
+</table>
+</center>
+</body>
+</html>
